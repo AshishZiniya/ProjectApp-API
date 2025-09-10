af5a6d9bb5e087f52feab9ce8b511287bea8e42d
Ziniya Ashish <98575396+AshishZiniya@users.noreply.github.com>
2025-09-10 18:42:48 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { Comment } from './comments/comment.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: Number(process.env.DB_PORT) || 6543,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
