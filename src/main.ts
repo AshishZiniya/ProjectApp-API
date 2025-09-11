94c1f3eff2e8a1d4cba236fce847880e7c800ec8
WDCS-Ashish <ashish.ziniya@webcluesinfotech.com>
2025-09-11 11:37:14 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,19 @@ async function bootstrap() {
   app.use(helmet());
   app.use(cookieParser());
   app.enableCors({
-    origin: ['project-app-indol.vercel.app', 'project-app-ashishziniyas-projects.vercel.app', 'project-l7qo10dv6-ashishziniyas-projects.vercel.app'],
+    origin: [
+      'project-app-indol.vercel.app',
+      'project-app-ashishziniyas-projects.vercel.app',
+      'project-l7qo10dv6-ashishziniyas-projects.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 10000, () => {
-    console.log(`Server is running on https://projectapp-api-k8mo.onrender.com`);
+    console.log(
+      `Server is running on https://projectapp-api-k8mo.onrender.com`,
+    );
   });
 }
 void bootstrap();
